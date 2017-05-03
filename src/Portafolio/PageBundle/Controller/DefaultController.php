675c77d967ffa68c34aca5abf1209e6bbe60fc3e
--global <--global>
2017-05-03 23:48:12 +0000

    +Agregado peticion para el uso del tunel entre el command y el handler

--- src/Portafolio/PageBundle/Controller/DefaultController.php
+++ src/Portafolio/PageBundle/Controller/DefaultController.php
@@ -2,13 +2,20 @@
 
 namespace Portafolio\PageBundle\Controller;
 
+
 use Portafolio\PageBundle\Entity\users;
+use Portafolio\PageBundle\UseCase\CreateUser\CreateUserCommand;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $command = new CreateUserCommand(["name"=>"pruebas de comando"]);
+        $response = $this->container->get('command_handler_connected')->execute($command);
+
+return new JsonResponse($response, 200);
         $list = $this->container->get('list_repositories')->getRepository('users');
 
         $user = new users();
