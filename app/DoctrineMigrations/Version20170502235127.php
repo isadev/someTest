f990b97447c0eb188ed4287d3f96f7117ea508b5
--global <--global>
2017-05-03 00:19:57 +0000

    migration para la tabla de users

---
+++ app/DoctrineMigrations/Version20170502235127.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170502235127 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE users (id INT NOT NULL, name VARCHAR(255) DEFAULT NULL, last_name VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, identity_card INT NOT NULL, nacionality VARCHAR(5) NOT NULL, email VARCHAR(255) NOT NULL, active VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+//        $this->addSql('CREATE TABLE address (id INT NOT NULL, city INT DEFAULT NULL, description VARCHAR(200) DEFAULT NULL, PRIMARY KEY(id))');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE users_id_seq CASCADE');
+        $this->addSql('DROP TABLE address');
+        $this->addSql('DROP TABLE users');
+    }
+}
