280db5e04e0ce69b8b91f89902d60f688c568ebd
--global <--global>
2017-05-03 00:19:33 +0000

    agregada clase para el manejo de repositorios

---
+++ src/Portafolio/PageBundle/Resources/Factory/RepositoryFactory.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: isa
+ * Date: 5/2/17
+ * Time: 9:48 PM
+ */
+
+namespace Portafolio\PageBundle\Resources\Factory;
+
+
+class RepositoryFactory
+{
+    private $em;
+
+    public function __construct($args)
+    {
+        $this->em = $args->getManager();
+    }
+
+    public function getRepository($name)
+    {
+        $position = null;
+        $repositories = [
+            [
+                'repositoryLocation' => 'PortafolioPageBundle:users',
+                'repositoryName' => 'users',
+            ],
+            [
+                'repositoryLocation' => 'PortafolioPageBundle:nombre_otra_entidad',
+                'repositoryName' => 'nombre_otra_entidad',
+            ],
+        ];
+
+        $position = array_search($name,array_column($repositories,'repositoryName'));
+
+        if (is_null($position))
+            return null;
+        return $this->em->getRepository($repositories[$position]['repositoryLocation']);
+    }
+}
