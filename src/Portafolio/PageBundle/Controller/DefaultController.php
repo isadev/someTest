22098ce3d77ef9db08c5f8db37ec319622b3aec5
--global <--global>
2017-05-03 00:17:33 +0000

    agregada extension para el listado de repositorios

--- src/Portafolio/PageBundle/Controller/DefaultController.php
+++ src/Portafolio/PageBundle/Controller/DefaultController.php
@@ -2,12 +2,27 @@
 
 namespace Portafolio\PageBundle\Controller;
 
+use Portafolio\PageBundle\Entity\users;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('PortafolioPageBundle:Default:index.html.pug');
+        $list = $this->container->get('list_repositories')->getRepository('users');
+
+        $user = new users();
+
+        $user->createUser(
+            [
+            "name" => "paquita",
+            "email" => "as123@123.com",
+            "last_name" => "del barrio",
+            "identity_card" => 123321321,
+            "address" => "y vive cerca"
+            ]);
+
+        $list->saveObj($user);
+        return $this->render('PortafolioPageBundle:Default:index.html.twig',['data' => count($list->findAll()), 'otro' => 'blablabla']);
     }
 }
