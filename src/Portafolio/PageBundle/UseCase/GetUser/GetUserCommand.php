30ec8a148e80864afa0580026adf883a17941bc9
--global <--global>
2017-05-13 15:15:15 +0000

    + Agregado caso de uso GetUser para la busqueda de un usuario en especifico

---
+++ src/Portafolio/PageBundle/UseCase/GetUser/GetUserCommand.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: isa
+ * Date: 5/13/17
+ * Time: 2:27 PM
+ */
+
+namespace Portafolio\PageBundle\UseCase\GetUser;
+
+use Portafolio\PageBundle\Command\Command;
+use Portafolio\PageBundle\Service\CommandBase;
+
+class GetUserCommand extends CommandBase
+{
+
+}
---
+++ src/Portafolio/PageBundle/UseCase/GetUser/GetUserHandler.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: isa
+ * Date: 5/13/17
+ * Time: 2:53 PM
+ */
+
+namespace Portafolio\PageBundle\UseCase\GetUser;
+
+
+use Portafolio\PageBundle\Command\Command;
+use Portafolio\PageBundle\Command\Handler;
+use Portafolio\PageBundle\Resources\Factory\IRepositoryFactory;
+use Portafolio\PageBundle\Service\ResponseHandler;
+
+class GetUserHandler implements Handler
+{
+    /**
+     * Ejecuta las funciones solicitadas por el caso de uso
+     *
+     * @param IRepositoryFactory $repositoryFactory
+     * @param Command $command
+     * @return mixed
+     * @author Isabel Nieto <isabelcnd@gmail.com>
+     * @version 03/05/2017
+     */
+    public function execute(IRepositoryFactory $repositoryFactory, Command $command)
+    {
+        $request = $command->getRequest();
+
+        $list = $repositoryFactory->getRepository('users');
+
+        $count = count($list->findAll());
+        $dataUser = $list->findBy(['name' => $request['name']]);
+        $data['name'] = $dataUser[0]->getName();
+        $data['countTotal'] = $count;
+
+        return new ResponseHandler(200, 'OK', $data, "ok");
+    }
+}
