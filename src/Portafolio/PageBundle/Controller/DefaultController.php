5ab4037ce7b5c21e8f71b85c1caed6bac733e272
--global <--global>
2017-05-14 22:15:38 +0000

    + Agregada nueva ruta y action para separar la renderizacion de los datos

--- src/Portafolio/PageBundle/Controller/DefaultController.php
+++ src/Portafolio/PageBundle/Controller/DefaultController.php
@@ -7,10 +7,32 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Portafolio\PageBundle\Service\ApplyCommand;
 
+/**
+ * Class DefaultController
+ * @package Portafolio\PageBundle\Controller
+ */
 class DefaultController extends Controller
 {
-    public function createUsersAction(Request $request)
-    {
+    /**
+     * Funcion encargada de la renderizacion de la vista principal
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @author Isabel Nieto <isabelcnd@gmail.com>
+     * @version 2017/05/14
+     */
+    public function indexAction() {
+        return $this->render('PortafolioPageBundle:Default:index.html.twig');
+    }
+
+    /**
+     * Funcion encargada de crear un usuario dado
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @author Isable Nieto <isabelcnd@gmail.com>
+     * @version 2017/05/14
+     */
+    public function createUsersAction(Request $request) {
         $data = json_decode($request->getContent(), true);
 
         $command = new ApplyCommand("User","Create");
@@ -19,11 +41,23 @@ class DefaultController extends Controller
         return new JsonResponse($response->getStatus(), $response->getCode());
     }
 
-    public function indexAction()
-    {
-        $command = new ApplyCommand("User","Get");
-        $response = $this->container->get('bus.request')->execute($command->setData(["name"=>"isabel"]));
+    /**
+     * Funcion encargada de buscar un usuario
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @author Isabel Nieto <isabelcnd@gmail.com>
+     * @version 2017/05/14
+     */
+    public function getUserAction(Request $request) {
+        $data = json_decode($request->getContent(), true);
 
-        return $this->render('PortafolioPageBundle:Default:index.html.twig',['data' => $response->getData()['name'], 'otro' => 'blablabla']);
+        $command = new ApplyCommand("User","Get");
+        $response = $this->container->get('bus.request')->execute($command->setData($data));
+
+        if ($response->getCode() == 200)
+            return new JsonResponse($response->getData(), $response->getCode());
+        else
+            return new JsonResponse($response->getMessage(), $response->getCode());
     }
 }
