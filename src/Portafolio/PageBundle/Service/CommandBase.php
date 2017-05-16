8f5c651da413def3f0b7f7d8405dd6aeba99b12d
jrequena <joel.2005.2@gmail.com>
2017-05-16 07:12:28 -0400

    % Refactory de CommandBase.

--- src/Portafolio/PageBundle/Service/CommandBase.php
+++ src/Portafolio/PageBundle/Service/CommandBase.php
@@ -18,32 +18,64 @@ use Portafolio\PageBundle\Command\Command;
  */
 class CommandBase implements Command
 {
-    private $attributes;
+   /**
+    * Constructor de la clase
+    * 
+    * @param array $data
+    */
+   public function __construct($data = null)
+   {
+       if (!empty($data) && is_array($data)) {
+           $this->setAtrributes($data);
+       }
+   }
 
-    /**
-     * Constructor de la clase
-     *
-     * Command constructor.
-     * @param $data
-     * @author Isabel Nieto <isabelcnd@gmail.com>
-     */
-    public function __construct($data)
-    {
-        $this->attributes = $data;
-    }
+   /**
+    * La función retorna el valor de un atributo del Command
+    * 
+    * @param  string $att atributo a buscar
+    * @return mixed valor del atributo
+    */
+   public function get($att)
+   {
+       return ( isset($this->$att) ? $this->$att : null );
+   }
 
-    /**
-     * Datos enviados desde el controller
-     * @return mixed
-     * @author Isabel Nieto <isabelcnd@gmail.com>
-     */
-    public function getRequest()
-    {
-        $array_response = [];
+   /**
+    * La función actualiza un valor de caso de uso
+    * 
+    * @param string $att atributo a actualizar
+    * @param [type] $val valor a actualizar
+    */
+   public function set($att,$val)
+   {
+       $this->$att = $val;
+   }
 
-        foreach ($this->attributes as $key => $value) {
-                $array_response["$key"] = $value;
-        }
-        return $array_response;
-    }
+   public function setAtrributes($data)
+   {
+       if (is_array($data)) {
+           foreach($data as $att => $val) {
+               $this->set($att,$val);
+           }
+           return true;
+       }
+       return false;
+   }
+   
+   /**
+    * La función retorna un arreglo con todos los valores
+    * del command
+    * 
+    * @return array arreglo con todos los valores del array
+    */
+   public function getRequest()
+   {
+       $response = [];
+       foreach($this as $att => $val)
+       {
+           $response[$att] = $val;
+       }
+       return $response;
+   }
 }
