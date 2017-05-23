ad4fa1c7c77c5262abe6ed70dc2616f9011a85b5
--global <--global>
2017-05-23 20:45:21 +0000

    + Agregada manejo de FOSUser

---
+++ app/DoctrineMigrations/Version20170523164750.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170523164750 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE fos_user (
+          id INT NOT NULL, 
+          username VARCHAR(180) NOT NULL, 
+          username_canonical VARCHAR(180) NOT NULL, 
+          email VARCHAR(180) NOT NULL, 
+          email_canonical VARCHAR(180) NOT NULL, 
+          enabled BOOLEAN NOT NULL, 
+          salt VARCHAR(255) DEFAULT NULL, 
+          password VARCHAR(255) NOT NULL, 
+          last_login TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, 
+          confirmation_token VARCHAR(180) DEFAULT NULL, 
+          password_requested_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, 
+          roles TEXT NOT NULL, 
+          PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_957A647992FC23A8 ON fos_user (username_canonical)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_957A6479A0D96FBF ON fos_user (email_canonical)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_957A6479C05FB297 ON fos_user (confirmation_token)');
+        $this->addSql('COMMENT ON COLUMN fos_user.roles IS \'(DC2Type:array)\'');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP TABLE fos_user');
+    }
+}
--- src/Portafolio/PageBundle/PortafolioPageBundle.php
+++ src/Portafolio/PageBundle/PortafolioPageBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PortafolioPageBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
