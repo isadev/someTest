00d46e67f63f22ab7b3eed982efcaa12aff4c8d1
isabel <isabelcnd@gmail.com>
2017-07-27 04:25:34 +0000

    % ultimas modificaciones

--- app/DoctrineMigrations/Version20170726132447.php
+++ app/DoctrineMigrations/Version20170726132447.php
@@ -18,7 +18,6 @@ class Version20170726132447 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('CREATE SEQUENCE viajero_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE agencia_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE viajes_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
